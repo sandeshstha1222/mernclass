9a6af3798b2e59511e5d3d05e05bbc1bdc1cee1a
sandeshstha1222 <sananddesh1029@gmail.com>
2023-01-07 17:21:21 +0545

    second

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import "./App.css";
 import Stock from "./Stock";
 import { STOCKS } from "./constant";
 import Counter from "./Counter";
+import { useState } from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 function App() {
+  const [stocks, setStocks] = useState(STOCKS);
+  const addStock = () => {
+    setStocks([
+      ...stocks,
+      {
+        securityId: "2790",
+        securityName: "Aarambha Chautari Laghubitta Bittiya Sanstha Limited",
+        symbol: "ACLBSL",
+        indexId: 58,
+        totalTradeQuantity: 315,
+        lastTradedPrice: 696,
+        percentageChange: -0.571428571,
+        lastUpdatedDateTime: "2022-12-20 14:52:52.330604",
+        previousClose: 700,
+      },
+    ]);
+  };
+  const removeStock = (givenID) => {
+    setStocks(stocks.filter((a) => a.securityId !== givenID));
+  };
   return (
     <div className="App">
       <Counter />
-      {STOCKS.map((stock) => (
-        <Stock key={stock.securityId} {...stock} />
-      ))}
-
+      <div style={{ margin: 20 }}>
+        {stocks.map((stock) => (
+          <Stock key={stock.securityId} {...stock} />
+        ))}
+      </div>
+      <AiOutlinePlusCircle color={"Black"} size={33} onClick={addStock} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/Stock.js
+++ src/Stock.js
@@ -1,12 +1,22 @@
 import React from "react";
-
+import { BsTrash } from "react-icons/bs";
 const Stock = ({ securityName, symbol, lastTradedPrice, ...rest }) => {
   console.log(rest, "in stock");
   return (
-    <div>
-      <h1>{securityName}</h1>
-      <h2>{symbol}</h2>
-      <h3>{lastTradedPrice}</h3>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        width: 700,
+        margin: "20px 0",
+      }}
+    >
+      {/* <span>{securityName}</span> */}
+      <span>{symbol}</span>
+      <div>
+        <span>{lastTradedPrice}</span>
+        <BsTrash onClick={() => {}} />
+      </div>
     </div>
   );
 };
